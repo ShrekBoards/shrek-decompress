affdb8762dfbd4c0891b8303a033ba7f6a02f046
Alex Kerr <kirby@cpan.org>
2017-08-21 21:33:34 +0100

    An attempt at cross-compatibility

--- shrek_decompress.h
+++ shrek_decompress.h
@@ -3,7 +3,12 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef _WIN32
 __declspec(dllexport) unsigned int shrek_compress(uint8_t *compressed, size_t comp_size, uint8_t *decompressed, size_t decomp_size);
 __declspec(dllexport) unsigned int shrek_decompress(uint8_t *decompressed, size_t decomp_size, uint8_t *compressed, size_t comp_size);
+#else
+unsigned int shrek_compress(uint8_t *compressed, size_t comp_size, uint8_t *decompressed, size_t decomp_size);
+unsigned int shrek_decompress(uint8_t *decompressed, size_t decomp_size, uint8_t *compressed, size_t comp_size);
+#endif
 
 #endif
